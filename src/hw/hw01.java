eb41a9d012f35163cdbd3956258ff9aaa1728f06
q9002285117 <105021038@live.asia.edu.tw>
2016-11-02 01:09:15 +0800

    hw

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,30 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw01 {
+
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println(fun(scn.nextInt(), scn.nextInt()));
+	}
 
+	public static float fun(int a, int b) {
+		// TODO Auto-generated method stub
+		float n=0;
+		switch (b){
+		case 1:
+			n=a*5/9+32;
+		break;
+		case 2:
+			n=(a-32)*((float)5/9);
+		break;
+	}
+		
+		return n;
 	}
 
 }
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,26 @@
 package hw;
-
+import java.util.Scanner;
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Scanner scn=new Scanner(System.in);
+System.out.println("請輸入身高、性別");
+System.out.println(fun(scn.nextInt(), scn.nextInt()));
 	}
 
+	public static float fun(float a, int b) {
+		// TODO Auto-generated method stub
+		
+		float n=0;
+		switch (b){
+		case 1:
+			n=(float)((a-170)*0.6+62);
+		break;
+		case 2:
+			n=(float)((a-158)*0.5+52);
+		break;
+	}
+return n;
+}
 }
